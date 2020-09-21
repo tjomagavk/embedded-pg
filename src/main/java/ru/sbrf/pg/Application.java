1bddf5701e5144794b3e511cc0c189cba119958b
Онищенко Артём Игоревич <AIOnishchenko.SBT@sberbank.ru>
2020-09-21 16:23:13 +0300

    pg zonky

--- src/main/java/ru/sbrf/pg/Application.java
+++ src/main/java/ru/sbrf/pg/Application.java
@@ -1,21 +1,12 @@
 package ru.sbrf.pg;
 
+import io.zonky.test.db.postgres.embedded.EmbeddedPostgres;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
 import javax.sql.DataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.DependsOn;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import ru.yandex.qatools.embed.postgresql.PostgresExecutable;
-import ru.yandex.qatools.embed.postgresql.PostgresProcess;
-import ru.yandex.qatools.embed.postgresql.PostgresStarter;
-import ru.yandex.qatools.embed.postgresql.config.AbstractPostgresConfig;
-import ru.yandex.qatools.embed.postgresql.config.PostgresConfig;
-import ru.yandex.qatools.embed.postgresql.distribution.Version;
 
 /**
  * @author Onishenko Artem
@@ -24,52 +15,20 @@ import ru.yandex.qatools.embed.postgresql.distribution.Version;
 @SpringBootApplication
 public class Application {
 
-    private static final List<String> DEFAULT_ADDITIONAL_INIT_DB_PARAMS = Arrays.asList("--nosync", "--locale=en_US.UTF-8");
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
     @Bean
-    @DependsOn("postgresProcess")
-    public DataSource dataSource(PostgresConfig config) {
-
-        DriverManagerDataSource ds = new DriverManagerDataSource();
-        ds.setDriverClassName("org.postgresql.Driver");
-        ds.setUrl(String.format("jdbc:postgresql://%s:%s/%s", config.net().host(), config.net().port(), config.storage().dbName()));
-        ds.setUsername(config.credentials().username());
-        ds.setPassword(config.credentials().password());
+    public DataSource dataSource() throws IOException {
+        int port = 15000;
+        DataSource ds = EmbeddedPostgres.builder()
+                                        .setPort(port)
+                                        .start()
+                                        .getPostgresDatabase();
+        log.warn("PG embedded start on {} port", port);
 
         return ds;
-    }
-
-    /**
-     * @param config the PostgresConfig configuration to use to start Postgres db process
-     * @return PostgresProcess , the started db process
-     */
-    @Bean(destroyMethod = "stop")
-    public PostgresProcess postgresProcess(PostgresConfig config) throws IOException {
-        log.error("user.home = {}", System.getProperty("user.home"));
-        PostgresStarter<PostgresExecutable, PostgresProcess> runtime = PostgresStarter.getDefaultInstance();
-        PostgresExecutable exec = runtime.prepare(config);
-        return exec.start();
-    }
-
-    /**
-     * @return PostgresConfig that contains embedded db configuration like user name , password
-     */
-    @Bean
-    public PostgresConfig embeddedPostgresConfig() throws IOException {
-        // make it readable from configuration source file or system , it is hard coded here for explanation purpose only
-        final PostgresConfig postgresConfig = new PostgresConfig(
-            Version.V10_6,
-            new AbstractPostgresConfig.Net("localhost", 15000),
-            new AbstractPostgresConfig.Storage("test"),
-            new AbstractPostgresConfig.Timeout(),
-            new AbstractPostgresConfig.Credentials("user", "pass")
-        );
-        postgresConfig.getAdditionalInitDbParams().addAll(DEFAULT_ADDITIONAL_INIT_DB_PARAMS);
-        return postgresConfig;
     }
 }
 
